405ccc35b3dc246e13a3fde2dcc060d57940a4e9
bismansahni <bismansahni@outlook.com>
2024-08-01 19:43:28 +0530

    best one till now

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -58,15 +58,15 @@ return (
           <form>
               <div className="form-group">
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" placeholder="Name" />
+                <input type="text" id="name" name="name" />
               </div>
               <div className="form-group">
                 <label htmlFor="email">Email Address</label>
-                <input type="email" id="email" name="email" placeholder="Email Address" />
+                <input type="email" id="email" name="email"  />
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Password" />
+                <input type="password" id="password" name="password"  />
               </div>
               {/* <button type="submit">Register</button> */}
             </form>
