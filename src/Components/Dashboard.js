3787b26a0b13230859e379b6f86cd2bfc3ca4eea
bismansahni <bismansahni@outlook.com>
2024-08-05 00:49:08 +0530

    added division for dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,99 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import '../styles/Dashboard.css';
-// import { getProfile } from '../services/api'; // Import getProfile function
-
-// const Dashboard = () => {
-//   const navigate = useNavigate();
-//   const [dropdownVisible, setDropdownVisible] = useState(false);
-//   const [animation, setAnimation] = useState('');
-//   const [profileData, setProfileData] = useState(null);
-
-//   const toggleDropdown = () => {
-//     setDropdownVisible(!dropdownVisible);
-//   };
-
-//   const handleLogout = () => {
-//     localStorage.removeItem('token');
-//     navigate('/login');
-//   };
-
-//   useEffect(() => {
-//     const fetchProfileData = async () => {
-//       const token = localStorage.getItem('token');
-//       if (token) {
-//         try {
-//           const profile = await getProfile(token);
-//           setProfileData(profile);
-//         } catch (error) {
-//           console.error('Failed to fetch profile data:', error);
-//         }
-//       }
-//     };
-
-//     fetchProfileData();
-//   }, []);
-
-//   const handleProfileSettings = () => {
-//     setAnimation('slide-left');
-//     setTimeout(() => navigate('/update-profile', { state: { profileData } }), 500); // Redirect after animation
-//   };
-  
-
-//   const dropdownItems = [
-//     { name: 'Profile Settings', action: handleProfileSettings },
-//     { name: 'Logout', action: handleLogout },
-//   ];
-
-//   return (
-//     <div className={`dashboard-main-container ${animation}`}>
-//       <div className="dashboard-box">
-//         <div className="navbar"> 
-//           <div className="navbar-left"></div>
-//           <div className="navbar-right">
-//             <h2>
-//               Account Settings
-//               <span 
-//                 className={`dropdown-arrow ${dropdownVisible ? 'rotate' : ''}`}
-//                 onClick={toggleDropdown}
-//               >
-//                 ▼
-//               </span>
-//             </h2>
-//             <div className={`dropdown ${dropdownVisible ? 'show-dropdown' : ''}`}>
-//               {dropdownItems.map((item, index) => (
-//                 <button
-//                   key={index}
-//                   onClick={item.action}
-//                   className="dropdown-button"
-//                 >
-//                   {item.name}
-//                 </button>
-//               ))}
-//             </div>
-//           </div>
-//         </div>
-//         <div className="middle-box">
-//           <h1>Get Referred to Your Dream Job!</h1>
-//           <h1>Become a Referrer and Help Others Succeed!</h1>
-//         </div>
-//         <div className="bottom-box">
-//           <div className="left-box">
-//             <button onClick={() => navigate('/be-a-referrer',{ state: { profileData } })}>Be a Referrer</button>
-//           </div>
-//           <div className="right-box">
-//           <button onClick={() => navigate('/get-referred', { state: { profileData } })}>Get Referred</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
-
-
-
 
 
 
@@ -141,12 +45,12 @@ const Dashboard = () => {
           <div className="navbar-left"></div>
           <div className="navbar-right">
             <h2>
-              Account Settings
+              {/* Account Settings */}
               <span 
                 className={`dropdown-arrow ${dropdownVisible ? 'rotate' : ''}`}
                 onClick={toggleDropdown}
               >
-                ▼
+                Account Settings ▼
               </span>
             </h2>
             <div className={`dropdown ${dropdownVisible ? 'show-dropdown' : ''}`}>
@@ -163,8 +67,13 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="middle-box">
-          <h1>Get Referred to Your Dream Job!</h1>
-          <h1>Become a Referrer and Help Others Succeed!</h1>
+        <div className="middle-box-left">
+        <h1>Become a Referrer and Help Others Succeed!</h1>
+        </div>
+         <div className="middle-box-right">
+         <h1>Get Referred to Your Dream Job!</h1>
+         
+          </div>
         </div>
         <div className="bottom-box">
           <div className="left-box">
